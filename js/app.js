672ddc3230df11d349ad00245b4545dcd4d67af3
christian8aguirre <christian8.aguirre@icloud.com>
2018-08-01 09:55:54 -0500

    D23_activity--async-fetch-and-render 2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 const request = superagent;
 var API_REQUEST = 'https://randomuser.me/api/?results=50';
 var divParent = document.querySelector('.users-list');
-request
-  .get(API_REQUEST)
-  .then(function (nElements) {
-    var elements = nElements.body.results;
-    elements.forEach(function (element) {
-      var article = document.createElement('article');
-      var span = document.createElement('span');
-      var h6 = document.createElement('h6');
-      span.innerHTML = element.nat;
-      h6.innerHTML = element.email;
-      article.appendChild(span);
-      article.appendChild(h6);
-      divParent.appendChild(article);
-    });
+var btn = document.querySelector('#app-container button');
+
+    btn.addEventListener('click', function (e) {
+      request
+        .get(API_REQUEST)
+        .then(function (nElements) {
+          var elements = nElements.body.results;
+            divParent.innerHTML = "";
+            elements.forEach(function (element) {
+              var article = document.createElement('article');
+              var span = document.createElement('span');
+              var h6 = document.createElement('h6');
+              span.innerHTML = element.nat;
+              h6.innerHTML = element.email;
+              article.appendChild(span);
+              article.appendChild(h6);
+              divParent.appendChild(article);  
+              });
+    
+          });
   })
